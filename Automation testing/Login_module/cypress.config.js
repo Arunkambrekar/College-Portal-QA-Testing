7ab1a3a5341416f86bde25118ba4127b430681bd
Arunkambrekar <arunkabrekar@gmail.com>
2025-08-05 10:42:06 +0530

    WIP: Local changes before pulling remote

--- Automation testing/Login_module/cypress.config.js
+++ Automation testing/Login_module/cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  reporter: 'mochawesome',
+  reporter: 'mochawesome', // You can switch to 'allure-mocha' if needed later
   reporterOptions: {
     reportDir: 'cypress/reports/mochawesome',
     overwrite: false,
@@ -10,8 +11,10 @@ module.exports = defineConfig({
   },
   e2e: {
     setupNodeEvents(on, config) {
-      // You can add event hooks here if needed later
+      // Register Allure writer
+      allureWriter(on, config);
+
+      return config;
     },
   },
 });
-
